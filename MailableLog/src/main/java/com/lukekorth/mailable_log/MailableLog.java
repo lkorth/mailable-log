69d1f8b457107892175d3bb3559e211716c13cbe
lkorth <luke@lukekorth.com>
2014-12-09 23:25:09 -0800

    Add initial JavaDoc

--- MailableLog/src/main/java/com/lukekorth/mailable_log/MailableLog.java
+++ MailableLog/src/main/java/com/lukekorth/mailable_log/MailableLog.java
@@ -27,14 +27,42 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.FileAppender;
 
+/**
+ * MailableLog handles the initialization of logging and also provides a method to build
+ * an {@link android.content.Intent} that points to the log so it may be sent via email or other means.
+ *
+ * The {@link #init(android.content.Context, boolean)} method should be called from the
+ * {@link android.app.Application#onCreate()} method to setup the log.
+ */
 public class MailableLog {
 
+    /**
+     * The default pattern for writing to the log. To write your own pattern
+     * see <a href="http://logback.qos.ch/manual/layouts.html#ClassicPatternLayout">Logback's documentation</a>.
+     */
     public static final String DEFAULT_PATTERN = "%date{MMM dd | HH:mm:ss.SSS} %highlight(%-5level) %-25([%logger{36}]) %msg%n";
 
+    /**
+     * Called to initialize logging. Should be called in {@link android.app.Application#onCreate()}
+     *
+     * @param context
+     * @param writeToLogcat If {@code true} log messages will be written to logcat as well as
+     *                      to disk. If {@code false} log messages will only be written to disk.
+     *                      Apps should only ever write to logcat in development.
+     */
     public static void init(Context context, boolean writeToLogcat) {
         init(context, writeToLogcat, DEFAULT_PATTERN);
     }
 
+    /**
+     * Called to initialize logging. Should be called in {@link android.app.Application#onCreate()}
+     *
+     * @param context
+     * @param writeToLogcat If {@code true} log messages will be written to logcat as well as
+     *                      to disk. If {@code false} log messages will only be written to disk.
+     *                      Apps should only ever write to logcat in development.
+     * @param pattern The log pattern to use. The default is {@link #DEFAULT_PATTERN}
+     */
     public static void init(Context context, boolean writeToLogcat, String pattern) {
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         loggerContext.reset();
@@ -65,10 +93,32 @@ public class MailableLog {
         }
     }
 
+    /**
+     * Get the path to the log file
+     *
+     * @param context
+     * @return A {@code String} representation of the path of the log file
+     */
     public static String getFilePath(Context context) {
         return context.getFileStreamPath("mailable.log").getAbsolutePath();
     }
 
+    /**
+     * Called to build an Intent that can be used to send an email.
+     * Upon receiving the {@link android.content.Intent} from {@link #buildEmailIntent(android.content.Context, String, String, String, String)}
+     * you can call {@code context.startActivity(Intent.createChooser(intent, String))}
+     * to display a chooser to the user.
+     *
+     * @param context
+     * @param emailAddress The email address the log should be sent to
+     * @param emailSubject The email subject
+     * @param fileName The filename for the log to be attached to the email
+     * @param metaData A {@code String} that will be prepended to the log. New lines should be
+     *                 seperated with a "\n"
+     * @return An {@link android.content.Intent} to be passed to {@link android.content.Intent#createChooser(android.content.Intent, CharSequence)}
+     *         which in turn can be passed to {@link android.content.Context#startActivity(android.content.Intent)}
+     * @throws IOException When MailableLog fails to read or write the log file
+     */
     public static Intent buildEmailIntent(Context context, String emailAddress, String emailSubject,
                                         String fileName, String metaData) throws IOException {
         File file = getFile(context, fileName);
